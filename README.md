52bf5f5767061a5864cda80ab146935c2c446273
Ramon Lapenta <ramono@gmail.com>
2014-02-16 11:13:05 +0000

    Added modifier code
    Added add on files

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-Statamic-plural-modifier
+Statamic plural modifier
 ========================
 
 A modifier add on to pluralise variables
+
+This function was ported from an Inflector class found in the Akelos Framework.
+
+### Usage
+	{{ variable|plural }}
+
+
---
+++ _add-ons/plural/mod.plural.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Modifier_plural
+ * Converts a variable to plural
+ *
+ * @author  Ramon Lpenta
+ * @author  Yang Yang
+ */
+class Modifier_plural extends Modifier
+{
+    function index($value)
+    {
+        $plural = array(
+        '/(quiz)$/i' => '1zes',
+        '/^(ox)$/i' => '1en',
+        '/([m|l])ouse$/i' => '1ice',
+        '/(matr|vert|ind)ix|ex$/i' => '1ices',
+        '/(x|ch|ss|sh)$/i' => '1es',
+        '/([^aeiouy]|qu)ies$/i' => '1y',
+        '/([^aeiouy]|qu)y$/i' => '1ies',
+        '/(hive)$/i' => '1s',
+        '/(?:([^f])fe|([lr])f)$/i' => '12ves',
+        '/sis$/i' => 'ses',
+        '/([ti])um$/i' => '1a',
+        '/(buffal|tomat)o$/i' => '1oes',
+        '/(bu)s$/i' => '1ses',
+        '/(alias|status)/i'=> '1es',
+        '/(octop|vir)us$/i'=> '1i',
+        '/(ax|test)is$/i'=> '1es',
+        '/s$/i'=> 's',
+        '/$/'=> 's');
+
+        $uncountable = array('equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep');
+
+        $irregular = array(
+        'person' => 'people',
+        'man' => 'men',
+        'child' => 'children',
+        'sex' => 'sexes',
+        'move' => 'moves');
+
+        $lowercased_value = strtolower($value);
+
+        foreach ($uncountable as $_uncountable){
+            if(substr($lowercased_value,(-1*strlen($_uncountable))) == $_uncountable){
+                return $value;
+            }
+        }
+
+        foreach ($irregular as $_plural=> $_singular){
+            if (preg_match('/('.$_plural.')$/i', $value, $arr)) {
+                return preg_replace('/('.$_plural.')$/i', substr($arr[0],0,1).substr($_singular,1), $value);
+            }
+        }
+
+        foreach ($plural as $rule => $replacement) {
+            if (preg_match($rule, $value)) {
+                return preg_replace($rule, $replacement, $value);
+            }
+        }
+        return false;
+
+    }    
+}
